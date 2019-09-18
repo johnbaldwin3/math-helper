70bf8318bd29b4e489ffbee9862c7c4b27c8b857
Baldwin <JBaldwin@advanceamerica.net>
2019-09-18 15:32:57 -0400

    updated math prog, architecture of store

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,48 +2,61 @@
   <v-container>
     <v-row class="home">
       <v-row>
-        <v-flex>
+        <v-col>
           Home component
-        </v-flex>
+          <v-row><v-btn @click="createNewProblem">Click</v-btn></v-row>
+        </v-col>
       </v-row>
       <v-col>
-        <v-flex xs12>
+        <v-col xs12>
           <v-select
             :items="numbers"
-            v-model="multiplicand"
-            label="Multiplicand"
+            v-model="multiplicandMaxVar"
+            label="Multiplicand MAX"
             @change="handleProduct()"
           >
           </v-select>
-        </v-flex>
-        <v-flex xs12>
+        </v-col>
+        <v-col xs12>
           <v-select
             :items="numbers"
-            v-model="multiplier"
-            label="Multiplier"
+            v-model="multiplierMaxVar"
+            label="Multiplier MAX"
             @change="handleProduct()"
           >
           </v-select>
-        </v-flex>
-        
+        </v-col>
+        <v-col xs12>
+          <v-select
+            :items="timerOptions"
+            v-model="timerLimitMax"
+            label="Time Limit"
+            @change="handleTime()"
+          >
+          </v-select>
+        </v-col>
       </v-col>
       <v-col column>
-        <v-flex>
+        <v-col>
           {{ multiplicand }}
-        </v-flex>
-        <v-flex>
+        </v-col>
+        <v-col>
           {{ multiplier }}
-        </v-flex>
-        <v-flex>
+        </v-col>
+        <v-col>
           -------
-        </v-flex>
-        <v-flex>
-          {{ product }} {{ Math.floor(Math.random() * Math.floor(12)) }}
-        </v-flex>
+        </v-col>
+        <v-col>
+          {{ product }}
+        </v-col>
       </v-col>
       
     </v-row>
-   
+    <v-row>
+      <v-col>
+        {{ timeLimit / 1000 }} second(s)
+      </v-col>
+    </v-row>
 
 
     <!-- Choose Options -->
@@ -55,19 +68,50 @@
 </template>
 
 <script>
+import { mapActions, mapGetters } from "vuex";
+
   export default {
     name: "Home",
     data() {
       return {
         numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-        multiplicand: 0,
-        product: this.handleProduct(),
-        multiplier: 0,
+        multiplicandMaxVar: 0,
+        productVar: this.handleProduct(),
+        multiplierMaxVar: 0,
+        multiplicandVar: 0,
+        multiplierVar: 0,
+        timerLimitMax: 0,
+        timerOptions: [{ text: '30 seconds', value: 30000}, {text: '1 min', value: 60000}, {text: '1.5 minutes', value: '90000'}, {text: '2 mins', value: '120000'}]
       }
     },
+    computed: {
+      ...mapGetters([
+          "timeLimit",
+          "multiplicand",
+          "multiplier",
+          "product",
+          "score",
+          "scoreRecords",
+          "timer",
+          "timeLeft"
+        ])
+    },
     methods: {
+      ...mapActions([
+        "setMultiplicandMax",
+        "setMultiplierMax",
+        "setTimerLength",
+        "setMultiplicand",
+        "setMultiplier",
+        "getProduct",
+        "createNewProblem"
+      ]),
       handleProduct() {
-        this.product = this.multiplicand * this.multiplier;
+        this.setMultiplicandMax(this.multiplicandMaxVar);
+        this.setMultiplierMax(this.multiplierMaxVar);
+      },
+      handleTime() {
+        this.setTimerLength(this.timerLimitMax)
       }
     }
   }
--- src/store.js
+++ src/store.js
@@ -12,9 +12,80 @@ export default new Vuex.Store({
     multiplierMax: 0,
     product: 0,
     score: 0,
-    timer: 0,
+    scoreRecords: [],
+    timerLength: 0,
+    timeLeft: 0,
     increment: 0
   },
-  mutations: {},
-  actions: {}
+  mutations: {
+    SET_MAX_MULTIPLICAND: (state, payload) => {
+      state.multiplicandMax = payload;
+    },
+    SET_MAX_MULTIPLIER: (state, payload) => {
+      state.multiplierMax = payload;
+    },
+    SET_MULTIPLIER: state => {
+      state.multiplier = Math.floor(Math.random() * Math.floor(state.multiplierMax + 1));
+    },
+    SET_MULTIPLICAND: state => {
+      state.multiplicand = Math.floor(Math.random() * Math.floor(state.multiplicandMax + 1));
+    },
+    SET_TIMER: (state, payload) => {
+      state.timeLimit = payload;
+    },
+    START_TIMER: state => {
+
+    },
+    END_TIMER: state => {
+
+    },
+    SAVE_SCORE: (state, payload) => {
+      // eventually save score in DB with user name.
+      
+      // get user name
+
+      // get score
+
+      // add to list of scores
+    },
+    GET_PRODUCT: state => {
+      state.product = state.multiplicand * state.multiplier;
+    }
+
+  },
+  actions: {
+    setMultiplicandMax: ({ commit }, payload) => {
+      commit("SET_MAX_MULTIPLICAND", payload);
+    },
+    setMultiplierMax: ({ commit }, payload) => {
+      commit("SET_MAX_MULTIPLIER", payload);
+    },
+    setTimerLength: ({ commit }, payload) => {
+      commit("SET_TIMER", payload);
+    },
+    setMultiplicand: ({ commit }) => {
+      commit("SET_MULTIPLICAND");
+    },
+    setMultiplier: ({ commit }) => {
+      commit("SET_MULTIPLIER");
+    },
+    getProduct: ({ commit }) => {
+      commit("GET_PRODUCT");
+    },
+    createNewProblem: ({commit}) => {
+      commit("SET_MULTIPLIER");
+      commit("SET_MULTIPLICAND");
+      commit("GET_PRODUCT");
+    }
+  },
+  getters: {
+    timeLimit: state => state.timeLimit,
+    multiplicand: state => state.multiplicand,
+    multiplier: state => state.multiplier,
+    product: state => state.product,
+    score: state => state.score,
+    scoreRecords: state => state.stateRecords,
+    timer: state => state.timer,
+    timeLeft: state => state.timeLeft,
+  }
 });
